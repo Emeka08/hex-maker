89e9ba2fff7541ca1611d2678f34787a157fe34c
Emeka08 <ohaerifavour68@gmail.com>
2020-08-22 02:33:37 +0100

    changed the variable name

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 let body = document.getElementById('body');
 let button = document.getElementById('button');
 
-let hex = ['a','b','c','d','e','f',0,1,2,3,4,5,6,7,8,9];
+let hexDigits = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
 
-button.addEventListener('click', run);
+function createHex() {
 
-function run(){
-    let hexValue = "#";
-    for (i = 0; i < 6; i++) {
+    let hex = "#";
 
-        let randomHex = Math.floor(Math.random() * hex.length);
-        hexValue += hex[randomHex];
-        //console.log(randomHex);
-        console.log(hexValue);
+    for (let i = 0; i < 6; i++) {
+        
+        let randomHexNumber = Math.floor(Math.random() * hexDigits.length);
+        hex = hex + hexDigits[randomHexNumber];
+    };
 
-    }
-    //console.log(hexValue);
-    
-    body.style.backgroundColor = hexValue;
-}
+    body.style.backgroundColor = hex;
+
+};
+
+button.addEventListener('click', createHex);
+
+//setInterval(createHex, 1000);
